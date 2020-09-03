210c19219cfbd434ea42564614d86c6fe8cf69b8
guillaume verpoest <guillaume.verpoest@gmail.com>
2020-09-03 15:42:27 +0200

    Initial Commit

---
+++ krypton-ios/krypton_ios.swift
@@ -0,0 +1,11 @@
+//
+//  krypton_ios.swift
+//  krypton-ios
+//
+//  Created by Guillaume Verpoest on 03/09/2020.
+//  Copyright © 2020 Guillaume Verpoest. All rights reserved.
+//
+
+class krypton_ios {
+
+}
